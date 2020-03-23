a04fae7cb21e7022a4e1f20233fbeb2440b10d80
Guilherme-Freschi <50874403+Guilherme-Freschi@users.noreply.github.com>
2020-03-23 20:41:59 -0300

    Update AppCofre.java

--- src/aula20200323/cofre/AppCofre.java
+++ src/aula20200323/cofre/AppCofre.java
@@ -1,17 +1,20 @@
 package aula20200323.cofre;
 
-public class Cofre {
-	private boolean aberto = false;
+public class AppCofre {
 	
-	public void abrir() {
-		aberto = true;
+	public static void main(String[] args) {
+		//Não é possível invocar o método a partir da classe pois ele não é estático!
+		//Cofre.fechar() 
+		
+		Cofre cofreDaSala = new Cofre();
+		Cofre cofreDoBanco = new Cofre();
+		
+		cofreDoBanco.abrir();
+		
+		cofreDaSala.abrir();
+		cofreDaSala.fechar();
+		
+		System.out.println("Da sala: "+ cofreDaSala.isAberto());
+		System.out.println("Do banco: "+ cofreDoBanco.isAberto());
 	}
-	public void fechar() {
-		aberto = false;
-	}
-	public boolean isAberto() {
-		//return false;
-		return aberto;
-	}
-
 }
