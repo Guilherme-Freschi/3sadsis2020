3a1d01f94420394261d05211b196a01f05e75888
Guilherme-Freschi <50874403+Guilherme-Freschi@users.noreply.github.com>
2020-03-23 20:40:53 -0300

    Update TestesComCofre.java

--- src/aula20200323/cofre/TestesComCofre.java
+++ src/aula20200323/cofre/TestesComCofre.java
@@ -1,14 +1,43 @@
 package aula20200323.cofre;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 class TestesComCofre {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void deveAbrirEFecharCofre() {
+		
+		Cofre cofreFechado = new Cofre();
+		assertEquals(false , cofreFechado.isAberto());
+		
+		cofreFechado.abrir();
+		assertEquals(true, cofreFechado.isAberto());
+		
+		cofreFechado.fechar();
+		assertEquals(false, cofreFechado.isAberto());
+		
+		Cofre cofre = new Cofre();
+		cofre.abrir();
+		assertEquals(true, cofre.isAberto());
+	}
+	
+	@Test
+	void deveAbrirEFecharCofreComSenha() {
+		CofreSeguro cofreSeguro = new CofreSeguro("12345");
+		
+		assertEquals(false, cofreSeguro.isAberto());
+		
+		
+		cofreSeguro.abrir("54321");
+		assertEquals(true, cofreSeguro.isAberto());
+		
+
+		CofreSeguro cofreComSenhaIncorreta = new CofreSeguro("12345");
+		cofreComSenhaIncorreta.abrir("54321");
+		
+		assertEquals(false, cofreComSenhaIncorreta.isAberto());
 	}
 
 }
