5ed7b99bc5c9e1b02dc5a9ef8c0639c80d21531a
Guilherme-Freschi <50874403+Guilherme-Freschi@users.noreply.github.com>
2020-04-06 21:15:19 -0300

    Update Pessoa.java

--- src/aula20200406/Pessoa.java
+++ src/aula20200406/Pessoa.java
@@ -1,51 +1,57 @@
-package aula20200406.tiposDeMetodos;
+package aula20200406;
 
 public class Pessoa {
 	private String nome;
-	private int idade;
+	private int idadeEmAnos;
 	private double peso;
 	private double altura;
-
 	
 	public double getIMC() {
-		return peso / (altura * altura);
+		return peso/(altura*altura);
 	}
 	
-	// Olha um getter aqui!
+	
+	// Getter
 	public String getNome() {
 		return this.nome;
 	}
-
-	// Olha um setter aqui!
+	
+	// Setter
 	public void setNome(String nome) {
+		if (nome.length() == 0 || nome.trim().length() <= 1 ){
+			throw new RuntimeException("Nome invalido!");
+		}
 		this.nome = nome;
 	}
-
+	
 	public double getAltura() {
 		return this.altura;
 	}
-
 	public void setAltura(double altura) {
+		if (altura <= 0.25) {
+			throw new RuntimeException("Altura invalida! A sua altura tem que ser maior que 25cm");
+		}
 		this.altura = altura;
 	}
-
-	public int getIdade() {
-		return this.idade;
+	
+	public int getIdadeEmAnos() {
+		return this.idadeEmAnos;
 	}
-
-	public void setIdade(int idade) {
-		this.idade = idade;
+	public void setIdadeEmAnos(int idadeEmAnos) {
+		this.idadeEmAnos = idadeEmAnos;
 	}
-
+	
 	public double getPeso() {
 		return this.peso;
 	}
-
+	
 	public void setPeso(double peso) {
-		if (peso <= 0) {
-			throw new RuntimeException("Peso deve ser maior que zero!");
+		if (peso <= 0 ) {
+			throw new RuntimeException("Peso invalido! peso tem que ser maior que 0");
 		}
+		
 		this.peso = peso;
 	}
+	
 
 }
