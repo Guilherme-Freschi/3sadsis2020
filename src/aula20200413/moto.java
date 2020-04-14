c86728756fac44fa17e467f7f835ba77933238a8
Guilherme-Freschi <50874403+Guilherme-Freschi@users.noreply.github.com>
2020-04-13 21:57:35 -0300

    Update moto.java

--- src/aula20200413/moto.java
+++ src/aula20200413/moto.java
@@ -1,71 +1,17 @@
-package aula20200406.tiposDeMetodos;
+package aula20200413.moto;
 
-public class Pessoa {
-	private String nome;
-	private int idade;
-	private double peso;
-	private double altura;
-
-	//Elaborar tambem um construtor para a classe Pessoa que receba os valores iniciais de seus atributos (nome, idade, peso e altura).
-	public Pessoa() {
-		setNome(nome);
-		setIdade(idade);
-		setPeso(peso);
-		setAltura(altura);
+public class moto {
+	
+	public static void main(String[] args) {
+		
+		moto honda = new moto();
+		
+		moto.setModelo("CG 160 Titan");
+		moto.setAno("2019");
+		moto.setPlaca(CBJ160);
+		
 	}
 	
 	
-	public double getIMC() {
-		return peso / (altura * altura);
-	}
-
-	// Olha um getter aqui!
-	public String getNome() {
-		return this.nome;
-	}
-
-	// Olha um setter aqui!
-	public void setNome(String nome) {
-		if (this.nome.length() < 1 || this.nome == null) {
-			//nao consegui usar o throw new runtimeException (ativem o log kk)
-			System.out.println(" O nome esta nulo ou muito curto  ");
-		} else {
-			this.nome = nome;
-		}
-	}
-
-	public double getAltura() {
-		return this.altura;
-	}
-
-	public void setAltura(double altura) {
-		if (this.altura < 25) {
-			//nao consegui usar o throw new runtimeException (ativem o log kk)
-			System.out.println(" A altura precisa ser maior que 25cm  ");
-		}
-		else {
-			this.altura = altura;
-		}
-
-	}
-
-	public int getIdade() {
-		return this.idade;
-	}
-
-	public void setIdade(int idade) {
-		this.idade = idade;
-	}
-
-	public double getPeso() {
-		return this.peso;
-	}
-
-	public void setPeso(double peso) {
-		if (peso <= 0) {
-			throw new RuntimeException("Peso deve ser maior que zero!");
-		}
-		this.peso = peso;
-	}
 
 }
