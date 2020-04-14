7ba62fcb49e0bf0ac8caa87048770adf6a8ec232
Guilherme-Freschi <50874403+Guilherme-Freschi@users.noreply.github.com>
2020-04-14 16:34:20 -0300

    Update animal.java

--- src/aula20200413/animal.java
+++ src/aula20200413/animal.java
@@ -5,7 +5,6 @@ public class animal {
 	private int vacinacao;
 	private String raca;
 	private int idade;
-	private double peso;
 	
 	// 2.1 Um construtor padrão;
 	public animal() {
@@ -18,7 +17,6 @@ public class animal {
 		setVacinacao(vacinacao);
 		setRaca(raca);
 		setIdade(idade);
-		setPeso(peso);
 	}
 	
 	// 2.3 Um método modificador;
@@ -33,75 +31,20 @@ public class animal {
 	
 	// 2.5 Um método de acesso cujo retorno NÂO seja o valor de um atributo, mas sim
 	// um valor CALCULADO em função de um valor de atributo.;
-	public int getAnoDeLançamento() {
-		return calculandoAnoDeLançamento();
-	}
-
-	public int getTemporadas() {
-		return temporadas;
-	}
-
-	public int getNota() {
-		return calcularNota();
-	}
-
-
-	public int calculandoAnoDeLançamento() {
-		return 2020 - anoDeLançamento;
-	}
-
-
-	public int calcularNota() {
-		return 100 - nota;
-	}
-}
-
-
-   
-  
 	private int calcularDataVacinacao() {
-          if(this.vacinacao <= 2020) {
+	  if(this.vacinacao <= 2020) {
             throw new RuntimeException("Vacinação vencida ou inválida ");
            }
-  
-       }   
-	private int calcularIdadegato() {
-		if (this.idade <= 0) {
-			throw new RuntimeException("Por favor informe a Idade");
-		}
-		return this.idade*pegarPesogato();
+	  return calcularDataVacinacao();
 	}
-
+	private int calcularIdadegato() {
+	  if (this.idade <= 0) {
+	    throw new RuntimeException("Por favor informe a Idade");
+	  }
+	  return this.idade*pegarPesogato();
+	}   
+	
 }
 
-
-
-	public String toString() {
-		return "animal [" + "nome:" + getNome() + ",nota:" + getNota() + " ,temporadas=" + getTemporadas()
-				+ ",Serie lançanda a " + getAnoDeLançamento() + " anos]";
-	}
-
 	
-
-	
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public void setAnoDeLançamento(int anoDeLançamento) {
-		this.anoDeLançamento = anoDeLançamento;
-	}
-
-	public void setTemporadas(int temporadas) {
-		this.temporadas = temporadas;
-	}
-
-	
-
-
-	public String getNome() {
-		return nome;
-	}
-
 	
