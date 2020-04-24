1c2eac15301fd98c87df858da8dea4f9082aa744
Guilherme-Freschi <50874403+Guilherme-Freschi@users.noreply.github.com>
2020-04-23 22:47:15 -0300

    Create AppCirculo.java

---
+++ src/segundaAep/AppCirculo.java
@@ -0,0 +1,9 @@
+package exercicio3e4;
+
+public class AppCirculo {
+    public static void main(String[] args) {
+            circulo c = new circulo(5);
+            c.setRaio(15);
+            c.exibeDados();
+    }
+}
