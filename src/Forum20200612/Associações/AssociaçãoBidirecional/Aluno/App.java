9666b562ffdd31e74ec94b72e07eb68debcc95f5
Guilherme-Freschi <50874403+Guilherme-Freschi@users.noreply.github.com>
2020-06-12 23:03:36 -0300

    Update App.java

--- src/Forum20200612/Associações/AssociaçãoBidirecional/Aluno/App.java
+++ src/Forum20200612/Associações/AssociaçãoBidirecional/Aluno/App.java
@@ -4,29 +4,29 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-public class TestaTurma {
+public class App {
 
     public static void main(String[] args) {
 
-        Aluno aluno1 = new Aluno("Jose Almeida");
-        Aluno aluno2 = new Aluno("Lucas Henrique");
-        Aluno aluno3 = new Aluno("Carlos Soares");
+        Aluno aluno1 = new Aluno("Victor Almeida");
+        Aluno aluno2 = new Aluno("Lucas Barbosa");
+        Aluno aluno3 = new Aluno("Mario Soares");
 
         List lista1 = new ArrayList();
         lista1.add(aluno1);
         lista1.add(aluno2);
 
-        Turma turma1 = new Turma("4ºA", lista1);
+        Turma turma1 = new Turma("2°", lista1);
 
         List lista2 = new ArrayList();
         lista2.add(aluno1);
         lista2.add(aluno3);
-        Turma turma2 = new Turma("6ºA", lista2);
+        Turma turma2 = new Turma("3°", lista2);
 
-        System.out.println("Alunos 4ºA");
+        System.out.println("Alunos 2°");
         System.out.println(turma1.getAlunos());
         System.out.println("");
-        System.out.println("Alunos 6ºA");
+        System.out.println("Alunos 3°");
         System.out.println(turma2.getAlunos());
 
     }
