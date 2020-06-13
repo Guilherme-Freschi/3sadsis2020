9276ee303c193ab6c97c23a817eccb7f62b858b1
Guilherme-Freschi <50874403+Guilherme-Freschi@users.noreply.github.com>
2020-06-12 22:09:41 -0300

    Create Pessoa.java

---
+++ src/Forum20200612/Associações/AssociaçãoUnidirecional/Pessoa.java
@@ -0,0 +1,17 @@
+package forum20200612.associacaoUnidirecional;
+
+public class Pessoa {
+	private CPF cpf;
+
+	public Pessoa(){
+		//por enquanto nao faz nada
+	}
+
+	public CPF getCpf(){
+		return cpf;
+	}
+
+	public void setCpf(CPF cpf){
+		this.cpf = cpf;
+	}
+}
