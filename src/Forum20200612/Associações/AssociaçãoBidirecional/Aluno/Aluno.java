2e7a9752d4acc56a6b2b8d480f4b619ac1533a55
Guilherme-Freschi <50874403+Guilherme-Freschi@users.noreply.github.com>
2020-06-12 22:54:36 -0300

    Create Aluno.java

---
+++ src/Forum20200612/Associações/AssociaçãoBidirecional/Aluno/Aluno.java
@@ -0,0 +1,38 @@
+public class Aluno {
+
+    private String nome;
+    private Turma turma;
+
+    public Aluno(String nome) {
+        this.nome = nome;
+    }
+
+    public void addTurma(Turma turma) {
+        if (this.turma == null) {
+            this.setTurma(turma);
+        } else {
+
+            System.out.println("Aluno " + getNome() + ", Já está matricula em uma turma");
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Turma getTurma() {
+        return turma;
+    }
+
+    public void setTurma(Turma turma) {
+        this.turma = turma;
+    }
+
+    public String toString() {
+        return "Aluno: " + nome + ", " + getTurma();
+    }
+}
