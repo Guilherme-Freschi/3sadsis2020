92c2e31ff656f645a6d089f3227c6251be133b8f
Guilherme-Freschi <50874403+Guilherme-Freschi@users.noreply.github.com>
2020-06-12 22:43:09 -0300

    Update App.java

--- src/Forum20200612/Associações/AssociaçãoBidirecional/App.java
+++ src/Forum20200612/Associações/AssociaçãoBidirecional/App.java
@@ -1,1 +1,12 @@
+package forum20200612.associacaoBidirecional;
 
+public class App {
+
+    public static void main(String[] args) {
+        Banda banda = new Banda();
+
+        Membro membro = new membro(banda);
+     
+    }
+
+}
