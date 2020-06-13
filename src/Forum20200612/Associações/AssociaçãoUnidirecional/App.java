a6596818fab2ae8cc7fa8cd024a9aef286bf5e45
Guilherme-Freschi <50874403+Guilherme-Freschi@users.noreply.github.com>
2020-06-12 22:08:36 -0300

    Create App.java

---
+++ src/Forum20200612/Associações/AssociaçãoUnidirecional/App.java
@@ -0,0 +1,11 @@
+package forum20200612.associacaoUnidirecional;
+
+public class App {
+
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+
+        CPF cpf = new CPF(pessoa);
+    }
+
+}
