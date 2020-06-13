8b5302fce1a554329e2bf56315e753dc0777f795
Guilherme-Freschi <50874403+Guilherme-Freschi@users.noreply.github.com>
2020-06-12 22:35:58 -0300

    Update Banda.java

--- src/Forum20200612/Associações/AssociaçãoBidirecional/Banda.java
+++ src/Forum20200612/Associações/AssociaçãoBidirecional/Banda.java
@@ -1,33 +1,6 @@
-package forum20200612.associacao0Para1;
+package forum20200612.associacaoBidirecional;
 
 public class Banda {
-	
-	public String nome;
-	public int numAlbum;
-        public Pais pais;
-	
-	
-	public Banda(String nome, Pais pais, int numAlbuns) {
-		this.nome = nome;
-		this.numAlbum = numAlbum;
-                this.pais = pais;
-                
-		
-	}
-	
-	public String getNome() {
-		return nome;
-	}
-	public int numAlbuns() {
-		return numAlbum;
-	}	
-        public Pais getPais() {
-		return pais;
-	}
-
-	public void setPais(Pais pais) {
-		this.pais = pais;
-	}
-
-   
-    }
+	private String nome;
+	private Membro membro;
+}
