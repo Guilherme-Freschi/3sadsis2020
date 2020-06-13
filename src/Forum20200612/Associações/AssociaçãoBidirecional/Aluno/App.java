a5242f9c8d00467cdbd6c53cec95078851c3d98f
Guilherme-Freschi <50874403+Guilherme-Freschi@users.noreply.github.com>
2020-06-12 22:55:26 -0300

    Create App.java

---
+++ src/Forum20200612/Associações/AssociaçãoBidirecional/Aluno/App.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class TestaTurma {
+
+    public static void main(String[] args) {
+
+        Aluno aluno1 = new Aluno("Jose Almeida");
+        Aluno aluno2 = new Aluno("Lucas Henrique");
+        Aluno aluno3 = new Aluno("Carlos Soares");
+
+        List lista1 = new ArrayList();
+        lista1.add(aluno1);
+        lista1.add(aluno2);
+
+        Turma turma1 = new Turma("4ºA", lista1);
+
+        List lista2 = new ArrayList();
+        lista2.add(aluno1);
+        lista2.add(aluno3);
+        Turma turma2 = new Turma("6ºA", lista2);
+
+        System.out.println("Alunos 4ºA");
+        System.out.println(turma1.getAlunos());
+        System.out.println("");
+        System.out.println("Alunos 6ºA");
+        System.out.println(turma2.getAlunos());
+
+    }
+
+}
